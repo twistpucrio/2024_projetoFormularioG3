4c7c50a06f1c393cf18d4e80df45d42c89f86e5d
Luizaregnier <Luizaregnier@tecgraf.puc-rio.br>
2024-08-15 15:48:26 -0300

    criado o nome das funções do javaScript

---
+++ arquivo.js
@@ -0,0 +1,26 @@
+//funcoes para fazer no javascript:
+// Não possibilitar preencher o nome do arquivo com caráteres especiais (exceto underscore);
+// Iniciar com letra maiúscula;
+// Deve ter o tamanho mínimo 8 caracteres e máximo 15
+//Os resultados podem ser informados utilizando mensagens de alertas.
+
+//func para limpar o forms
+function limpaForm()
+{
+   
+}
+
+function procuraCaracterEsp()
+{
+
+}
+
+function verificaMaiuscula()
+{
+
+}
+
+function verificaNumCaracter()
+{
+    
+}
